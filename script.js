c3f7130fa49cb38963d235bf9c8afb687075e852
mutiat80 <mutiatoladokun383@gmail.com>
2026-04-08 10:56:38 +0100

    using javascript yet to add more functionalities for schientific

---
+++ script.js
@@ -0,0 +1,32 @@
+const display = document.getElementById('display');
+
+/* function keword plus  function name (parameters){
+//function body
+// }
+function name(argument);
+
+*/
+
+function append (value){
+
+display.value += value;
+
+}
+
+function erase (){
+    display.value ='';
+}
+
+
+function calculation(){
+try{
+    display.value = eval(display.value)
+}
+catch(error){
+    display.value = 'error'
+}
+}
+
+function eraseLast(){
+    display.value = display.value.slice(0,-1)
+}
